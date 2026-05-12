ee1622c8bda4d574520d27a921466a568bd38c7b
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-05-12 20:54:29 +1200

    feat: restructure project by removing unused files and adding new layout and styles

---
+++ public/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
