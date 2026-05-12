a48e0bdc6082e555dc220d11f85f20402bdcca64
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-05-12 23:34:20 +1200

    feat: add Dialog component to Header for recipe creation functionality

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,15 @@
 import { raleway } from "@/public/fonts";
 import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog/dialog";
+import { Button } from "../ui/button/button";
+import google from "@/public/google-icon-logo-svgrepo-com.svg";
+import Image from "next/image";
 
 export default function Header() {
   return (
@@ -10,7 +20,31 @@ export default function Header() {
       >
         Itadaki
       </Link>
-      <div></div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="accent">Create Recipe</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogTitle className="mb-4 text-tertiary">
+            Wanna Cook Easier?
+          </DialogTitle>
+          <DialogDescription className="mb-6">
+            Itadaki is an app to reduce your time thinking of what recipes to
+            make. Just take a photo of your receipt of groceries; the app will
+            generate a recipe in seconds.
+          </DialogDescription>
+          <Button
+            variant="login"
+            className="w-full px-6"
+            centerText
+            icon={
+              <Image src={google} alt="Google Logo" height={16} width={16} />
+            }
+          >
+            <span>Continue with Google</span>
+          </Button>
+        </DialogContent>
+      </Dialog>
     </header>
   );
 }
