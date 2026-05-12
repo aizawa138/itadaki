fea10c532810994ed8ffa0617d617a166b710049
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-05-12 21:05:57 +1200

    feat: remove Montserrat font and update layout to use Inter font

--- public/fonts.ts
+++ public/fonts.ts
@@ -1,12 +1,6 @@
 import { Inter } from "next/font/google";
-import { Montserrat } from "next/font/google";
 
 export const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
-
-export const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "700"],
 });
