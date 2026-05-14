5018810982359ae24df5df15318eee6ea0980ff6
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-05-15 10:24:12 +1200

    feat: remove session check and redirect from Dashboard component

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,12 +1,3 @@
-import { getSession } from "@/src/lib/auth/get-session";
-import { redirect } from "next/navigation";
-
 export default async function Dashboard() {
-  const session = await getSession();
-
-  if (!session) {
-    redirect("/");
-  }
-
   return <div>Dashboard</div>;
 }
