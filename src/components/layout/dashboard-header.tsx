fed9f7a668eb9cfa3d7b26f1179b48ac9fede3fb
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-05-15 22:13:29 +1200

    feat: implement logout functionality in dashboard header

--- src/components/layout/dashboard-header.tsx
+++ src/components/layout/dashboard-header.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import logo from "@/public/Logo.svg";
 import Link from "next/link";
-import profile from "@/public/Profile.svg";
+import profile from "@/public/profile-circle-svgrepo-com.svg";
 import {
   DropdownMenuContent,
   DropdownMenuItem,
@@ -10,8 +12,17 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown/dropdown";
 import { Button } from "../ui/button/button";
+import signOut from "@/src/lib/auth/sign-out";
+import { useRouter } from "next/navigation";
 
 export default function DashboardHeader() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push("/");
+    router.refresh();
+  };
   return (
     <header className="bg-primary flex items-center justify-between border-b border-b-border sticky top-0 px-8 h-12">
       <Link href="/dashboard">
@@ -35,11 +46,11 @@ export default function DashboardHeader() {
             />
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent>
+        <DropdownMenuContent className="mr-2">
           <DropdownMenuItem>Settings</DropdownMenuItem>
           <DropdownMenuItem>Theme</DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Logout</DropdownMenuItem>
+          <DropdownMenuItem onClick={handleSignOut}>Logout</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenuRoot>
     </header>
