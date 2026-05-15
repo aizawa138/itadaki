f1fd202a52902b7b494fa8fc9fca2e2b098b90fa
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-05-15 15:25:35 +1200

    feat: add profile dropdown menu with settings and logout options

--- src/components/layout/dashboard-header.tsx
+++ src/components/layout/dashboard-header.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
 import logo from "@/public/Logo.svg";
 import Link from "next/link";
+import profile from "@/public/Profile.svg";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuRoot,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown/dropdown";
+import { Button } from "../ui/button/button";
 
 export default function DashboardHeader() {
   return (
-    <header className="bg-primary flex border-b border-b-border sticky top-0 px-8 h-12">
-      <Link href="/dashboard" className="flex items-center">
+    <header className="bg-primary flex items-center justify-between border-b border-b-border sticky top-0 px-8 h-12">
+      <Link href="/dashboard">
         <Image
           src={logo}
           alt="Itadaki logo"
@@ -14,6 +23,25 @@ export default function DashboardHeader() {
           loading="eager"
         />
       </Link>
+      <DropdownMenuRoot>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon">
+            <Image
+              src={profile}
+              alt="Profile icon"
+              width={32}
+              height={32}
+              loading="eager"
+            />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Theme</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenuRoot>
     </header>
   );
 }
