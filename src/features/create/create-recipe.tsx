beb9e030b5eb6c21d06b4e6b14a2420a6933383c
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-05-18 17:22:06 +1200

    feat: add options for creating recipes from receipts or pantry items
    - Introduced CreateOption component for selecting recipe creation mode.
    - Implemented state management for receipt and pantry selection.
    - Updated dialog content to include options for creating recipes from receipts or pantry.
    - Disabled the create recipe button until a mode is selected.

--- src/features/create/create-recipe.tsx
+++ src/features/create/create-recipe.tsx
@@ -8,8 +8,35 @@ import {
   DialogDescription,
 } from "@/src/components/ui/dialog/dialog";
 import { Button } from "@/src/components/ui/button/button";
+import CreateOption from "./create-option";
+import selectReceipt from "@/public/Select Receipt.svg";
+import selectPantry from "@/public/Select Pantry.svg";
+import { useState } from "react";
 
 export default function CreateRecipe() {
+  const [modeSelected, setModeSelected] = useState(false);
+  const [isReceipt, setIsReceipt] = useState(false);
+
+  const handleReceiptClick = () => {
+    if (isReceipt) {
+      setIsReceipt(false);
+      setModeSelected(false);
+    } else {
+      setIsReceipt(true);
+      setModeSelected(true);
+    }
+  };
+
+  const handlePantryClick = () => {
+    if (!isReceipt && modeSelected) {
+      setIsReceipt(false);
+      setModeSelected(false);
+    } else {
+      setIsReceipt(false);
+      setModeSelected(true);
+    }
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -17,14 +44,39 @@ export default function CreateRecipe() {
           + Create Recipe
         </Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="max-w-4xl">
         <DialogTitle className="mb-4 text-tertiary">How to create?</DialogTitle>
         <DialogDescription className="mb-6">
           Start off with uploading your receipt to the pantry. The app will
           generate a recipe depending on the items in your pantry.
         </DialogDescription>
-
-        <Button variant="secondary" size="default" className="w-full">
+        <div className="flex flex-col sm:flex-row gap-4 mb-4">
+          <CreateOption
+            src={selectReceipt}
+            title="From Receipts"
+            onClick={handleReceiptClick}
+            className={`${isReceipt && modeSelected ? "outline-2 outline-blue-500" : "outline-0"}`}
+          >
+            Create a recipe by scanning receipts that you have. The groceries on
+            your receipt will be sent to the pantry to create your recipe.
+          </CreateOption>
+          <CreateOption
+            src={selectPantry}
+            title="From Pantry"
+            onClick={handlePantryClick}
+            className={`${!isReceipt && modeSelected ? "outline-2 outline-blue-500" : "outline-0"}`}
+          >
+            Create a recipe by analyzing what ingredients you have. The
+            groceries in the pantry will be reduced once you have created the
+            recipe.
+          </CreateOption>
+        </div>
+        <Button
+          variant="secondary"
+          size="default"
+          className="w-full transition"
+          disabled={!modeSelected}
+        >
           Create Recipe
         </Button>
       </DialogContent>
